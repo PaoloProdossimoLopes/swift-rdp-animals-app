a08786643b844ee2023260c9bb9ce2f913a7e54e
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-04-24 20:43:47 -0300

    chore: setup xcodegen

---
+++ Animals/Animals/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Animals
+//
+//  Created by Paolo Prodossimo Lopes on 24/04/23.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
